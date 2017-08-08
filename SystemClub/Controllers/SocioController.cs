b0af3632ba9f690b595f9aef9cf5dd43e18e0f2c
André Perretto de Andrade <andre.andrade@intelecta.com.br>
2017-08-08 19:09:03 -0300

    Add project files.

---
+++ SystemClub/Controllers/SocioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SystemClub.Controllers
+{
+    public class SocioController : Controller
+    {
+        // GET: Socio
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
